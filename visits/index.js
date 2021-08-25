8c2183cac1f329e222bd39b477cf92c000d016b6
caplincapture <leuping@gmail.com>
2021-08-24 18:46:41 -0700

    assembling a dockerfile

--- visits/index.js
+++ visits/index.js
@@ -7,11 +7,11 @@ client.set('visits', 0);
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
+    res.send('Number of visits ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
 });
 
 app.listen(8081, () => {
-  console.log('Listening on port 8081');
+  console.log('listening on port 8081');
 });
